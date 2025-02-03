ba53b971141e1553564b2f0349a9404a1f301600
Iben-arch <borbok49@gmail.com>
2025-02-03 20:38:14 +0700

    update

--- lib/screen/editprofile_screen.dart
+++ lib/screen/editprofile_screen.dart
@@ -1,100 +1,129 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../components/oshi_avatar.dart';
 import 'setting_page.dart';
+import 'package:image_picker/image_picker.dart';
 
-class EditProfileScreen extends StatelessWidget {
+class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({
     super.key,
   });
+
+  @override
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  ImageProvider<Object>? _avatarImage = AssetImage('assets/profile/suisei.png');
+
+  Future<void> _pickImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _avatarImage = FileImage(File(image.path));
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
-          // Header Section
           Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            padding: EdgeInsets.all(10),
             decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30),
-              ),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
             ),
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
             child: Column(
               children: [
-                CircleAvatar(
-                  backgroundImage:
-                      AssetImage('assets/images/profile_image.jpg'),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Nora',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  'holoplus points  |  LV 1  |  0+',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.white70,
-                  ),
-                ),
-                SizedBox(height: 20),
                 Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        spreadRadius: 2,
-                      ),
-                    ],
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  padding: EdgeInsets.all(20),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
+                      Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          CircleAvatar(
+                            radius: 40,
+                            backgroundColor: Colors.white,
+                            backgroundImage: _avatarImage,
+                          ),
+                          CircleAvatar(
+                            radius: 17,
+                            backgroundColor: Colors.blue,
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.photo_outlined,
+                                color: Colors.white,
+                              ),
+                              iconSize: 20,
+                              onPressed: _pickImage,
+                              tooltip: 'Change Image',
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Nora',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'holoplus points   LV 1   0+',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Divider(
+                          height: 20,
+                          thickness: 1,
+                          color: Colors.grey.shade300),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text(
                             'My Oshi',
                             style: TextStyle(
-                              fontSize: 18,
+                              fontSize: 16,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
                           TextButton(
                             onPressed: () {},
-                            child: Text(
-                              'Edit',
-                              style: TextStyle(color: Colors.blue),
-                            ),
-                          ),
+                            child: Text('Edit',
+                                style: TextStyle(color: Colors.blue)),
+                          )
                         ],
                       ),
-                      SizedBox(height: 10),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: List.generate(
                           5,
                           (index) => CircleAvatar(
-                            radius: 30,
-                            backgroundColor: Colors.grey[300],
-                            backgroundImage: AssetImage(
-                              'assets/avatar_$index.png', // เปลี่ยนภาพตามที่คุณมี
-                            ),
+                            radius: 25,
+                            backgroundColor: Colors.grey.shade300,
+                            child: Icon(Icons.person, color: Colors.blue),
                           ),
                         ),
-                      ),
+                      )
                     ],
                   ),
                 ),
